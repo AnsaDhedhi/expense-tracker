8d67a218b4a5aa65dbb4bcbce20f97e3f9e99045
Ansa-Dhedhi <ansadhedhi98@gmail.com>
2020-06-22 01:40:58 +0500

    Initialize project using Create React App

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Header = () => {
+    return(
+        <h1 className="own_css">
+        <hr/>
+        
+                Expense Tracker 
+        by Ansa Dhedhi
+        
+        <hr/>
+        </h1>
+    )
+}
